497656063524ab3a0ceeb78be8cbc452ac234216
krishna.hoderker <krishna.hoderker@creativecapsule.com>
2019-01-22 18:50:29 +0530

    Web Optimization

--- Bungii.Test.Integration/src/test/java/com/bungii/runner/RunAutoSuite.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/runner/RunAutoSuite.java
@@ -14,7 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-@CucumberOptions(features = "target/test-classes/features/web", monochrome = true, tags = "@web and @sanity", plugin = {
+@CucumberOptions(features = "target/test-classes/features/web", monochrome = true, tags = "@web and @sanity1", plugin = {
         "pretty", "html:target/cucumber-report/single",
         "json:target/cucumber-report/single/cucumber.json",
         "rerun:target/cucumber-report/single/rerun.txt", "com.bungii.common.utilities.CustomFormatter"},
@@ -36,7 +36,7 @@ public class RunAutoSuite extends AbstractTestNGCucumberTests {
         System.setProperty("LogFilePath", "Results");
 
     String ClassName = this.getClass().getSimpleName();
-        if (Platform !="web") {
+        if(Platform =="ios" || Platform=="android") {
     String[] deviceList = device.split(",");
     //if mutiple devices are pass from maven then get class number and use that device for running that class
     if (deviceList.length > 1) {
