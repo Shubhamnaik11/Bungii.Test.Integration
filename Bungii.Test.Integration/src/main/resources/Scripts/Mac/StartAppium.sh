04aaaa95c9a8635928534bedf7344958f5dcae14
CREATIVECAPSULE\vishal.bagi <vishal.bagi@creativecapsule.com>
2019-03-01 18:15:54 +0530

    updated shell

--- Bungii.Test.Integration/src/main/resources/Scripts/Mac/StartAppium.sh
+++ Bungii.Test.Integration/src/main/resources/Scripts/Mac/StartAppium.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-sentence="$0"",device2"
+sentence="$1"",device2"
+echo "senetence $sentence"
 sentence=${sentence//,/$'\n'}
 for device in $sentence
 do
-#echo "$device"
 query=".connection.$device"
 echo "${query}"
-OUTPUT="$(cat $1 | jq ${query})"
+OUTPUT="$(cat $2 | jq ${query})"
 echo "${OUTPUT}"
 appium -p ${OUTPUT}  &
 done
