9a6d5efb83e5bdee324009478a8ce22a1452872f
CREATIVECAPSULE\vishal.bagi <vishal.bagi@creativecapsule.com>
2019-03-04 13:44:53 +0530

    updated shell script for ios

--- Bungii.Test.Integration/src/main/resources/Scripts/Mac/KillAppium.sh
+++ Bungii.Test.Integration/src/main/resources/Scripts/Mac/KillAppium.sh
@@ -1,1 +1,5 @@
-killall node
+#kill all appium process started by Node
+killall node /usr/local/bin/appium
+
+#Kill all node process
+#killall node
--- Bungii.Test.Integration/src/main/resources/Scripts/Mac/StartAppium.sh
+++ Bungii.Test.Integration/src/main/resources/Scripts/Mac/StartAppium.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
-sentence="$1"",device2"
-echo "senetence $sentence"
-sentence=${sentence//,/$'\n'}
-for device in $sentence
+#list of devices to consider , $1 is argument from command line
+devicelist="$1"",device2"
+echo "Devicelist : $devicelist"
+devicelist=${devicelist//,/$'\n'}
+for device in $devicelist
 do
-query=".connection.$device"
-echo "${query}"
-OUTPUT="$(cat $2 | jq ${query})"
-echo "${OUTPUT}"
-appium -p ${OUTPUT}  &
+
+#json parser expression to fetch Appium port and wda port
+queryAppiumPort=".connection.$device"
+querywdaPort=".$device.wdaLocalPort"
+
+# get Appium port and wda port
+APPIUMPORT="$(cat $2 | jq ${queryAppiumPort})"
+WDAPORT="$(cat $2 | jq ${querywdaPort})"
+
+#Log file name
+today=`date '+%Y_%m_%d__%H_%M_%S'`;
+filename="/Users/ccitesting/Documents/AppiumLogs/AppuimLogs_$today.log"
+
+#start Appium server with parameter
+echo "Starting Appium server forDevice :$device with details , WDAPORT:${WDAPORT} , APPIUMPORT:${APPIUMPORT} , filename:${filename} "
+appium -p ${APPIUMPORT} --webdriveragent-port ${WDAPORT} --log-timestamp --local-timezone --log ${filename} &
+
+#wait for 5 sec
+Sleep 5s
 done
