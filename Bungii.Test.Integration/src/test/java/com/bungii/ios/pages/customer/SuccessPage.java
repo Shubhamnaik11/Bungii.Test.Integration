624fbe01c5c101f2266c28c593180ecc1e048317
CREATIVECAPSULE\vishal.bagi <vishal.bagi@creativecapsule.com>
2019-03-19 13:39:16 +0530

    update

--- Bungii.Test.Integration/src/test/java/com/bungii/ios/pages/customer/SuccessPage.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/ios/pages/customer/SuccessPage.java
@@ -7,7 +7,7 @@ public class SuccessPage extends PageBase {
 	public WebElement Text_NavigationBar() { return findElement("//XCUIElementTypeNavigationBar/XCUIElementTypeOther", PageBase.LocatorType.XPath); }
 
 	public WebElement Text_BungiiPosted () { return findElement("Bungii Posted!", LocatorType.Name); }
-	public WebElement Button_Done() { return findElement("DONE", LocatorType.Name); }
+	public WebElement Button_Done(boolean ...ignoreException) { return findElement("DONE", LocatorType.Name,ignoreException); }
 
 
 }
