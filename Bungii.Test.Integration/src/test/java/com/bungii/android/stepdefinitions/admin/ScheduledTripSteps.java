9a9a840c0ecf4fe80ac0c30ebd225a61d1349750
CREATIVECAPSULE\vishal.bagi <vishal.bagi@creativecapsule.com>
2019-09-11 10:14:27 +0530

    corrected package name

--- Bungii.Test.Integration/src/test/java/com/bungii/android/stepdefinitions/admin/ScheduledTripSteps.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/android/stepdefinitions/admin/ScheduledTripSteps.java
@@ -6,7 +6,7 @@ package com.bungii.android.stepdefinitions.admin;
 import com.bungii.SetupManager;
 import com.bungii.common.core.DriverBase;
 import com.bungii.common.utilities.LogUtility;
-import com.bungii.ios.utilityfunctions.GeneralUtility;
+import com.bungii.android.utilityfunctions.GeneralUtility;
 import com.bungii.web.manager.ActionManager;
 import com.bungii.android.pages.admin.ScheduledTripsPage;
 import com.bungii.common.utilities.PropertyUtility;
