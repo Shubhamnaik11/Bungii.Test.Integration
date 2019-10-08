ade141d5ff48fbccfbdaddb73fe312b6889190fd
vbagi-cci <42644707+vbagi-cci@users.noreply.github.com>
2019-10-08 10:15:57 +0530

    added try catch

--- Bungii.Test.Integration/src/test/java/com/bungii/ios/stepdefinitions/driver/LogInSteps.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/ios/stepdefinitions/driver/LogInSteps.java
@@ -24,6 +24,7 @@ public class LogInSteps extends DriverBase {
     GeneralUtility utility = new GeneralUtility();
     @Given("^I am logged in as \"([^\"]*)\" driver$")
     public void i_am_logged_in_as_something_driver(String option) throws Throwable {
+                try {
         String phone, password;
         boolean shouldLoginSucessful;
         switch (option.toLowerCase()) {
@@ -57,6 +58,10 @@ public class LogInSteps extends DriverBase {
         }
         else {
             //TODO: specify failure here
+        }
+                } catch (Exception e) {
+            logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
+            error( "Step  Should be successful", "Error performing step,Please check logs for more details", true);
         }
     }
 
