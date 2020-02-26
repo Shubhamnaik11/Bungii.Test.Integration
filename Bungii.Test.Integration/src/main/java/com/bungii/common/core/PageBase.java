9d235fd44603e0b0c8e3bbb21c068b6cea496593
krishna.hoderker <krishna.hoderker@creativecapsule.com>
2020-02-26 13:55:59 +0530

    Test script fixes - Page Base

--- Bungii.Test.Integration/src/main/java/com/bungii/common/core/PageBase.java
+++ Bungii.Test.Integration/src/main/java/com/bungii/common/core/PageBase.java
@@ -194,9 +194,13 @@ public class PageBase {
                         //ignore exception
                         //or to do action
                     } else {
+                        error("Locator "+identifier+" by type "+locatorType+" should be displayed", "Locator "+identifier+" by type "+locatorType+" is not displayed. Please find screenshot for more details.",
+                                true);
                         throw new NoSuchElementException(identifier);
                     }
                 } else {
+                    error("Locator "+identifier+" by type "+locatorType+" should be displayed", "Locator "+identifier+" by type "+locatorType+" is not displayed. Please find screenshot for more details.",
+                            true);
                     throw new NoSuchElementException(identifier);
                 }
             }  finally {
