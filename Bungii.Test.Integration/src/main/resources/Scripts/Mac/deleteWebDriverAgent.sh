ce283300f8fa29bb709c9e7bfa33ada850f775b0
krishna.hoderker <krishna.hoderker@creativecapsule.com>
2020-03-13 16:56:18 +0530

    Test script fixes - Adding local wda port

--- Bungii.Test.Integration/src/main/resources/Scripts/Mac/deleteWebDriverAgent.sh
+++ Bungii.Test.Integration/src/main/resources/Scripts/Mac/deleteWebDriverAgent.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #list of devices to consider , $1 is argument from command line
 
-ios-deploy --id $1 --uninstall_only --bundle_id  com.facebook.WebDriverAgentRunner.xctrunner
+#ios-deploy --id $1 --uninstall_only --bundle_id  com.facebook.WebDriverAgentRunner.xctrunner
 #wait for 18 sec
 Sleep 18s
-ios-deploy --id $1 --uninstall_only --bundle_id  com.apple.test.WebDriverAgentRunner-Runner
+#ios-deploy --id $1 --uninstall_only --bundle_id  com.apple.test.WebDriverAgentRunner-Runner
 Sleep 18s
+idevicediagnostics -u $1 restart
+Sleep 60s
 done
