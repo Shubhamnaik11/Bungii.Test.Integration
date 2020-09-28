9b7b2960fc6c2c157b421206e3704f27f1348690
Gopal Sawant <65227160+gsawant-cci@users.noreply.github.com>
2020-09-28 21:07:01 +0530

    Regression Fixes for 1214 run

--- Bungii.Test.Integration/src/test/java/com/bungii/web/pages/admin/Admin_ReferralSourcePage.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/web/pages/admin/Admin_ReferralSourcePage.java
@@ -5,19 +5,19 @@ import org.openqa.selenium.WebElement;
 
 public class Admin_ReferralSourcePage extends PageBase {
 
-    public WebElement Header_Source () { return findElement("//th[text()='Source']", LocatorType.XPath); }
+    public WebElement Header_Source () { return findElement("//th[text()='Sources']", LocatorType.XPath); }
 
     public WebElement Header_AccountsCreated () { return findElement("//th[text()='Accounts Created']", LocatorType.XPath); }
 
     public WebElement Header_PercentageOfTotalAC () { return findElement("//tr/th[3][text()='Percentage of total']", LocatorType.XPath); }
 
-    public WebElement Header_TripsCompleted () { return findElement("//th[text()='Trips Completed']", LocatorType.XPath); }
+    public WebElement Header_TripsCompleted () { return findElement("//th[text()='Deliveries Completed']", LocatorType.XPath); }
 
     public WebElement Header_PercentageOfTotalTC () { return findElement("//tr/th[5][text()='Percentage of total']", LocatorType.XPath); }
 
     public WebElement Menu_ReferralSource(boolean... ignoreException) { return findElement("adminmenu-referralsource", LocatorType.Id,ignoreException); }
 
-    public WebElement Title_ReferralSourcePage (boolean...ignoreException) { return findElement("//h4[@class='page-header' and text()='Referral Source']", LocatorType.XPath,ignoreException); }
+    public WebElement Title_ReferralSourcePage (boolean...ignoreException) { return findElement("//h4[@class='page-header' and text()='Referral Sources']", LocatorType.XPath,ignoreException); }
 
     public WebElement Button_Search ( ) { return findElement("//button[text()='Search']", LocatorType.XPath); }
 
