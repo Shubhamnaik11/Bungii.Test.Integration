a4af7d95e8a4b6b7b906bf35eca3440eaf7ddafb
krishna.hoderker <krishna.hoderker@creativecapsule.com>
2020-10-02 16:10:06 +0530

    QA checkin

--- Bungii.Test.Integration/src/test/java/com/bungii/ios/pages/driver/HomePage.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/ios/pages/driver/HomePage.java
@@ -39,7 +39,7 @@ public class HomePage extends PageBase {
     //	public WebElement Text_NavigationBar () { return findElement("//XCUIElementTypeNavigationBar/XCUIElementTypeOther", PageBase.LocatorType.XPath); }
 
     //public WebElement Text_NavigationBar (boolean ...ignoreException) { return findElement("//XCUIElementTypeNavigationBar", PageBase.LocatorType.XPath,ignoreException); }
-    public WebElement Text_NavigationBar () { return findElement("//XCUIElementTypeNavigationBar", LocatorType.XPath); }
+    public WebElement Text_NavigationBar (boolean ...ignoreException) { return findElement("//XCUIElementTypeNavigationBar", LocatorType.XPath,ignoreException); }
     public WebElement NavigationBar_Text() {return findElement("XCUIElementTypeNavigationBar", LocatorType.ClassName); }
 
     //public WebElement Button_AppMenu () { return findElement("//XCUIElementTypeNavigationBar/XCUIElementTypeButton", PageBase.LocatorType.XPath); }
