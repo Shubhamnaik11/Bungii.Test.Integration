38451cf67b1b640db7e6816f7a5a561a94a161c0
krishna.hoderker <krishna.hoderker@creativecapsule.com>
2020-10-05 10:54:42 +0530

    QA checkin - General fixes

--- Bungii.Test.Integration/src/test/java/com/bungii/ios/utilityfunctions/GeneralUtility.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/ios/utilityfunctions/GeneralUtility.java
@@ -896,10 +896,10 @@ catch(Exception ex)
             }        //temp fixed
             logger.detail("Expected App Header After Switching : "+ appHeader);
             Thread.sleep(5000);
-            logger.detail(SetupManager.getDriver().getPageSource());
+            //logger.detail(SetupManager.getDriver().getPageSource());
             new GeneralUtility().handleIosUpdateMessage();
             new GeneralUtility().handleAppleIDVerification();
-            logger.detail(SetupManager.getDriver().getPageSource());
+           // logger.detail(SetupManager.getDriver().getPageSource());
             pass("Switch to : " + appName + " application on device instance",
                     "Switched to : " + appName + " application on device instance", true);
             cucumberContextManager.setFeatureContextContext("CURRENT_APPLICATION", appName.toUpperCase());
