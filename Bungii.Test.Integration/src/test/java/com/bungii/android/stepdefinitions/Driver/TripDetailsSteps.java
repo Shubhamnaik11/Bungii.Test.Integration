ac417a4782783dfb740491df3f8e5f30c3806fcb
sneha.naik <sneha.naik@creativecapsule.com>
2021-11-25 19:28:53 +0530

    Added try catch block

--- Bungii.Test.Integration/src/test/java/com/bungii/android/stepdefinitions/Driver/TripDetailsSteps.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/android/stepdefinitions/Driver/TripDetailsSteps.java
@@ -54,43 +54,57 @@ public class TripDetailsSteps extends DriverBase {
 
     @Then("^I should able to see \"([^\"]*)\" available trip$")
     public void i_should_able_to_see_something_available_trip(String strArg1) throws Throwable {
-        List<WebElement> listOfBungii = availableTripsPage.Image_SelectBungiis();
-        List<WebElement> listOfCustomerBungii = availableTripsPage.Row_CustomerTrips();
-        switch (strArg1) {
-            case "two":
-                testStepAssert.isTrue(listOfBungii.size() == 2, "There should be two available deliveries",listOfBungii.size()+" available deliveries are displayed");
-                break;
-            case "zero":
-                testStepAssert.isTrue(listOfBungii.size() == 0, "There should be zero available deliveries", listOfBungii.size()+" available deliveries are displayed.");
-                break;
-            case "two customer":
-                testStepAssert.isTrue(listOfCustomerBungii.size() == 2, "There should be two available app customer deliveries",listOfBungii.size()+" available app customer deliveries are displayed");
-                break;
-            case "old":
-                int count = Integer.parseInt(String.valueOf(cucumberContextManager.getScenarioContext("COUNT_OF_AVAILABLE_TRIPS")));
-                testStepAssert.isTrue(listOfBungii.size() == count, "There should be no additional available deliveries",listOfBungii.size()+" available deliveries are displayed");
-                break;
-            default:
-                throw new Exception(" UNIMPLEMENTED STEP");
+        try{
+            List<WebElement> listOfBungii = availableTripsPage.Image_SelectBungiis();
+            List<WebElement> listOfCustomerBungii = availableTripsPage.Row_CustomerTrips();
+            switch (strArg1) {
+                case "two":
+                    testStepAssert.isTrue(listOfBungii.size() == 2, "There should be two available deliveries",listOfBungii.size()+" available deliveries are displayed");
+                    break;
+                case "zero":
+                    testStepAssert.isTrue(listOfBungii.size() == 0, "There should be zero available deliveries", listOfBungii.size()+" available deliveries are displayed.");
+                    break;
+                case "two customer":
+                    testStepAssert.isTrue(listOfCustomerBungii.size() == 2, "There should be two available app customer deliveries",listOfBungii.size()+" available app customer deliveries are displayed");
+                    break;
+                case "old":
+                    int count = Integer.parseInt(String.valueOf(cucumberContextManager.getScenarioContext("COUNT_OF_AVAILABLE_TRIPS")));
+                    testStepAssert.isTrue(listOfBungii.size() == count, "There should be no additional available deliveries",listOfBungii.size()+" available deliveries are displayed");
+                    break;
+                default:
+                    error("UnImplemented Step or incorrect available delivery count", "Incorrect available delivery count");
+                    break;
+            }
+        }
+        catch(Exception e){
+            logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
+            error("Step  Should be successful", "Error performing step,Please check logs for more details", true);
         }
     }
 
     @Then("^I should able to see \"([^\"]*)\" scheduled trip$")
     public void i_should_able_to_see_something_scheduled_trip(String strArg1) throws Throwable {
-        List<WebElement> listOfScheduledTrip = scheduledBungiiPage.List_ScheduledBungiis();
-        switch (strArg1) {
-            case "two":
-                testStepVerify.isTrue(listOfScheduledTrip.size() == 2, "There should be two scheduled trips","There is/are "+listOfScheduledTrip.size()+" Scheduled trips");
-                break;
-            case "one":
-                testStepVerify.isTrue(listOfScheduledTrip.size() == 1, "There should be one scheduled trip", "There is/are "+listOfScheduledTrip.size()+" Scheduled trips");
-                break;
-            case "zero":
-                testStepVerify.isTrue(listOfScheduledTrip.size() == 0, "There should be two scheduled trip", "There is/are "+listOfScheduledTrip.size()+" Scheduled trips");
-                break;
-            default:
-                throw new Exception(" UNIMPLEMENTED STEP");
+        try{
+            List<WebElement> listOfScheduledTrip = scheduledBungiiPage.List_ScheduledBungiis();
+            switch (strArg1) {
+                case "two":
+                    testStepVerify.isTrue(listOfScheduledTrip.size() == 2, "There should be two scheduled trips","There is/are "+listOfScheduledTrip.size()+" Scheduled trips");
+                    break;
+                case "one":
+                    testStepVerify.isTrue(listOfScheduledTrip.size() == 1, "There should be one scheduled trip", "There is/are "+listOfScheduledTrip.size()+" Scheduled trips");
+                    break;
+                case "zero":
+                    testStepVerify.isTrue(listOfScheduledTrip.size() == 0, "There should be two scheduled trip", "There is/are "+listOfScheduledTrip.size()+" Scheduled trips");
+                    break;
+                default:
+                    error("UnImplemented Step or incorrect scheduled delivery count", "Incorrect scheduled delivery count");
+            }
         }
+        catch(Exception e){
+            logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
+            error("Step  Should be successful", "Error performing step,Please check logs for more details", true);
+        }
+
     }
 
     @And("^I Count the number of available bungiis$")
