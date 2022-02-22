c6afff8dc41f64b3d59d6c070397e1cabc4d4fe4
sbhise-cci <sweta.bhise@creativecapsule.com>
2022-02-22 19:03:04 +0530

    Added try-catch block

--- Bungii.Test.Integration/src/test/java/com/bungii/web/stepdefinitions/admin/Admin_ReasonCodeSteps.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/web/stepdefinitions/admin/Admin_ReasonCodeSteps.java
@@ -38,71 +38,59 @@ public class Admin_ReasonCodeSteps extends DriverBase {
 
     @And("^I click on \"([^\"]*)\" in the dropdown$")
     public void i_click_on_something_in_the_dropdown(String dropdown) throws Throwable {
-        switch (dropdown){
+        try{
+        switch (dropdown) {
             case "Edit":
-                try {
                     action.click(admin_EditScheduledBungiiPage.Button_Edit());
-                }
-                catch(Exception e){
-                    logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
-                    error("Step should be successful", "Error performing step,Please check logs for more details",
-                            true);
-                }
-                break;
-                
-            case "Customer initiated":
+                    break;
 
-                Select selectCustomer = new Select((WebElement)admin_EditScheduledBungiiPage.Reason_Dropdown());
-                selectCustomer.selectByVisibleText("Customer initiated");
-                break;
+            case "Customer initiated":
+                    Select selectCustomer = new Select((WebElement) admin_EditScheduledBungiiPage.Reason_Dropdown());
+                    selectCustomer.selectByVisibleText("Customer initiated");
+                    break;
 
             case "Partner initiated":
-
-                Select selectPartner = new Select((WebElement)admin_EditScheduledBungiiPage.Reason_Dropdown());
-                selectPartner.selectByVisibleText("Partner initiated");
-                break;
+                    Select selectPartner = new Select((WebElement) admin_EditScheduledBungiiPage.Reason_Dropdown());
+                    selectPartner.selectByVisibleText("Partner initiated");
+                    break;
 
             case "Delivery Details":
-                action.click(admin_EditScheduledBungiiPage.Button_Delivery_Details());
-                break;
-
-
-            default:
-                throw new IllegalStateException("Unexpected value: " + dropdown);
+                    action.click(admin_EditScheduledBungiiPage.Button_Delivery_Details());
+                    break;
+        }
+        }
+        catch (Exception e){
+            logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
+            error("Step Should be successful", "Error in viewing result set",
+                    true);
         }
     }
 
     @And("^I click on the \"([^\"]*)\" and select future time$")
     public void i_click_on_the_something_and_select_future_time(String scheduleDate) throws Throwable {
-        switch (scheduleDate){
+        try{
+        switch (scheduleDate) {
             case "Time":
-                try {
                     action.click(admin_EditScheduledBungiiPage.TimePicker_Time());
                     Thread.sleep(1000);
                     action.click(admin_EditScheduledBungiiPage.ScheduledDate_Time_Dropdown3());
-                    String timeChanged=admin_EditScheduledBungiiPage.TimePicker_Time().getText();
-                    cucumberContextManager.setScenarioContext("Time_Changed",timeChanged);
-                }
-                catch(Exception e){
-                    logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
-                    error("Step should be successful", "Error performing step,Please check logs for more details",
-                            true);
-                }
-                break;
+                    String timeChanged = admin_EditScheduledBungiiPage.TimePicker_Time().getText();
+                    cucumberContextManager.setScenarioContext("Time_Changed", timeChanged);
+                    break;
             case "Date":
-                try {
+
                     action.click(admin_EditScheduledBungiiPage.DatePicker_ScheduledDate());
                     Thread.sleep(1000);
                     action.click(admin_EditScheduledBungiiPage.Changed_Date());
-                    String dateChanged=admin_EditScheduledBungiiPage.DatePicker_ScheduledDate().getText();
-                    cucumberContextManager.setScenarioContext("Date_Changed",dateChanged);
-                }
-                catch(Exception e){
-                    logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
-                    error("Step should be successful", "Error performing step,Please check logs for more details",
-                            true);
-                }
-                break;
+                    String dateChanged = admin_EditScheduledBungiiPage.DatePicker_ScheduledDate().getText();
+                    cucumberContextManager.setScenarioContext("Date_Changed", dateChanged);
+                    break;
+        }
+        }
+        catch (Exception e) {
+            logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
+            error("Step Should be successful", "Error in viewing result set",
+                    true);
         }
 
     }
@@ -123,7 +111,14 @@ public class Admin_ReasonCodeSteps extends DriverBase {
 
     @And("^I check if the \"([^\"]*)\" field is hidden$")
     public void i_check_if_the_something_field_is_hidden(String strArg1) throws Throwable {
-      testStepAssert.isFalse(admin_EditScheduledBungiiPage.Reason_Dropdown().isDisplayed(),"Reason should not be displayed","Reason is displayed");
+        try {
+            testStepAssert.isFalse(admin_EditScheduledBungiiPage.Reason_Dropdown().isDisplayed(), "Reason should not be displayed", "Reason is displayed");
+        }
+        catch(Exception e){
+            logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
+            error("Step should be successful", "Error performing step,Please check logs for more details",
+                    true);
+        }
     }
 
     @And("^I click on \"([^\"]*)\" for change time and check reason dropdown values$")
@@ -145,7 +140,6 @@ public class Admin_ReasonCodeSteps extends DriverBase {
                 Options.add(options);
             }
             testStepAssert.isTrue(Options.containsAll(expectedOptions),"Correct reasons need to be display","Correct reasons are display","Incorrect reasons are displayed");
-
         }
         catch(Exception e){
             logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
@@ -160,45 +154,68 @@ public class Admin_ReasonCodeSteps extends DriverBase {
             admin_EditScheduledBungiiPage.Reason_Dropdown();
         } catch (org.openqa.selenium.NoSuchElementException e) {
             testStepAssert.isTrue(true,"Reasons is not displayed","Reasons is not displayed","Reasons is displayed");
-
         }
     }
 
     @And("^I click on \"([^\"]*)\" and add \"([^\"]*)\" driver$")
     public void i_click_on_something_and_add_something_driver(String strArg1, String driverName) throws Throwable {
+        try{
         action.click(admin_EditScheduledBungiiPage.TextBox_DriverSearch());
         action.sendKeys(admin_EditScheduledBungiiPage.TextBox_DriverSearch(),driverName);
         Thread.sleep(1000);
         action.sendKeys(admin_EditScheduledBungiiPage.TextBox_DriverSearch()," ");
         action.JavaScriptClick(admin_EditScheduledBungiiPage.Driver_Dropdown_Result(driverName));
         Thread.sleep(1000);
-
+        }
+        catch (Exception e){
+            logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
+            error("Step should be successful", "Error performing step,Please check logs for more details",
+                    true);
+        }
     }
 
     @When("^I click \"([^\"]*)\" on scheduled delivery details page$")
     public void i_click_something_on_scheduled_delivery_details_page(String strArg1) throws Throwable {
-        action.click(admin_EditScheduledBungiiPage.Changed_Time());
-        Thread.sleep(5000);
+        try {
+            action.click(admin_EditScheduledBungiiPage.Changed_Time());
+            Thread.sleep(5000);
+        }
+        catch(Exception e){
+            logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
+            error("Step should be successful", "Error performing step,Please check logs for more details",
+                    true);
+        }
     }
 
 
     @Then("^the updated time should be displayed on delivery details page$")
     public void the_updated_time_should_be_displayed_on_delivery_details_page() throws Throwable {
-        String expectedTime = (String) cucumberContextManager.getScenarioContext("Time_Changed");
-        String actualTime = action.getText(admin_EditScheduledBungiiPage.Changed_Time());
-        testStepAssert.isTrue(actualTime.contains(expectedTime),"Correct time need to be display","Correct time is display","Incorrect time is displayed");
+        try {
+            String expectedTime = (String) cucumberContextManager.getScenarioContext("Time_Changed");
+            String actualTime = action.getText(admin_EditScheduledBungiiPage.Changed_Time());
+            testStepAssert.isTrue(actualTime.contains(expectedTime), "Correct time need to be display", "Correct time is display", "Incorrect time is displayed");
+        }
+        catch(Exception e){
+            logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
+            error("Step should be successful", "Error performing step,Please check logs for more details",
+                    true);
+        }
+
     }
 
     @Then("^the updated date should be displayed on delivery details page$")
     public void the_updated_date_should_be_displayed_on_delivery_details_page() throws Throwable {
-
-        String expectedDate = (String) cucumberContextManager.getScenarioContext("Date_Changed");
-        String actualDate = action.getText(admin_EditScheduledBungiiPage.Changed_Time());
-        testStepAssert.isTrue(actualDate.contains(expectedDate),"Correct date need to be display","Correct date is display","Incorrect date is displayed");
+        try {
+            String expectedDate = (String) cucumberContextManager.getScenarioContext("Date_Changed");
+            String actualDate = action.getText(admin_EditScheduledBungiiPage.Changed_Time());
+            testStepAssert.isTrue(actualDate.contains(expectedDate), "Correct date need to be display", "Correct date is display", "Incorrect date is displayed");
+        }
+        catch(Exception e){
+            logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
+            error("Step should be successful", "Error performing step,Please check logs for more details",
+                    true);
+        }
 
     }
-
-
-
 
 }
