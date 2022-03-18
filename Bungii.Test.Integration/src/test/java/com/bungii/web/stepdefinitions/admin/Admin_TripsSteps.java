826cbeb3be4f1fdb986132cb494f8e46c1a25400
allan.fernandes <allan.fernandes@creativecapsule.com>
2022-03-18 17:50:14 +0530

    Feature/Core - 2047 QA Checkin - Verify admin is able to edit customer notes in admin portal

--- Bungii.Test.Integration/src/test/java/com/bungii/web/stepdefinitions/admin/Admin_TripsSteps.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/web/stepdefinitions/admin/Admin_TripsSteps.java
@@ -357,7 +357,7 @@ public class Admin_TripsSteps extends DriverBase {
             String driver = driver1;
             if (tripType[0].equalsIgnoreCase("duo"))
                 driver = driver1 + "," + driver2;
-            if (status.equalsIgnoreCase("Scheduled") || status.equalsIgnoreCase("Searching Drivers") || status.equalsIgnoreCase("Driver Removed")|| status.equalsIgnoreCase("Driver(s) Not Found")) {
+            if (status.equalsIgnoreCase("Scheduled") || status.equalsIgnoreCase("Searching Drivers") || status.equalsIgnoreCase("Driver Removed")|| status.equalsIgnoreCase("Driver(s) Not Found")||status.equalsIgnoreCase("Assigning Driver(s)") ) {
                 String xpath = String.format("//td[contains(.,'%s')]/following-sibling::td[contains(.,'%s')]/following-sibling::td[5]", tripType[0].toUpperCase(), customer);
                 String costPath =  String.format("//td[contains(.,'%s')]/preceding-sibling::td[1]/span", customer);
                 TripPath= xpath;
@@ -816,6 +816,7 @@ try{
         String Display_Change_DropOff = action.getText(admin_TripDetailsPage.Text_DropOff_Location());
         //testStepVerify.isEquals(Expected_Change_DropOff,Display_Change_DropOff);
         testStepAssert.isTrue(Display_Change_DropOff.contains(Expected_Change_DropOff),"Correct address need to display","Correct address is display","Incorrect address is displayed");
+        testStepAssert.isFalse(Expected_Change_DropOff.contentEquals((String) cucumberContextManager.getScenarioContext("OLD_DROPOFF_LOCATION")),"Dropoff Address should be changed","Dropoff address is changed","Dropoff address is not changed");
         log(" I confirm the change drop off address on delivery details page",
                 "I have confirmed the change drop off address on delivery details page", false);
     } catch(Exception e){
@@ -906,12 +907,25 @@ try{
         }
     }
 
-    @Then("^I confirm Pickup note is updated$")
-    public void i_confirm_pickup_note_is_updated() throws Throwable {
+    @And("^I confirm Pickup note is \"([^\"]*)\"$")
+    public void i_confirm_pickup_note_is_something(String noteStatus) throws Throwable {
         try{
-        String Change_Pickup_Note = (String) cucumberContextManager.getScenarioContext("Change_Pickup_Note");
-        String Display_Pickup_Note = action.getText(admin_EditScheduledBungiiPage.Text_Pickup_Note());
-        testStepVerify.isEquals(Change_Pickup_Note,Display_Pickup_Note);
+            String Display_Pickup_Note = action.getText(admin_EditScheduledBungiiPage.Text_Pickup_Note());
+            String Change_Pickup_Note = (String) cucumberContextManager.getScenarioContext("Change_Pickup_Note");
+            switch (noteStatus){
+                case "Updated":
+                    Thread.sleep(1000);
+                    testStepVerify.isEquals(Change_Pickup_Note,Display_Pickup_Note);
+                    testStepAssert.isFalse(Display_Pickup_Note.contentEquals((String) cucumberContextManager.getScenarioContext("OLD_ADDITION_NOTE")),"Edited note should be displayed","Edited note should be displayed","Edited note is not displayed");
+                 break;
+                case "Added":
+                    Thread.sleep(1000);
+                    testStepAssert.isEquals(Change_Pickup_Note,Display_Pickup_Note,"Admin added note should be displayed","Admin added note is  displayed","Admin added note is not displayed");
+                    break;
+                case "Deleted":
+                    break;
+            }
+
     } catch(Exception e){
         logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
         error("Step should be successful", "Error performing step,Please check logs for more details",
@@ -985,8 +999,10 @@ try{
     public void i_edit_the_drop_off_address() throws Throwable {
         try{
         testStepAssert.isElementDisplayed(admin_ScheduledTripsPage.Label_Drop_Off_Location(),"Drop off location should display","Drop off location is display","Drop off location is not display");
+        Thread.sleep(1000);
+        cucumberContextManager.setScenarioContext("OLD_DROPOFF_LOCATION",action.getText(admin_ScheduledTripsPage.DropOff_Address()));
+        cucumberContextManager.setScenarioContext("OLD_ADDITION_NOTE",action.getText(admin_EditScheduledBungiiPage.Text_Additional_Note()));
         action.click(admin_ScheduledTripsPage.Button_Edit_Drop_Off_Address());
-
         log("I edit the drop off address ",
                 "I have edited the dropoff address ");
     } catch(Exception e){
@@ -2167,5 +2183,45 @@ try{
                     true);
         }
     }
+
+    @Then("^I should see \"([^\"]*)\" field empty$")
+    public void i_should_see_something_field_empty(String notetype) throws Throwable {
+        try{
+        switch (notetype){
+            case "Additional Notes":
+                Thread.sleep(1000);
+                String addNote = action.getText(admin_EditScheduledBungiiPage.Text_Additional_Note());
+                testStepAssert.isTrue(addNote.length() <1,"Additional notes field should be empty","Additional notes field is empty","Additional notes field is not empty");
+                break;
+            case "Additional Instructions":
+                String addInstruction = action.getText(admin_EditScheduledBungiiPage.Text_Additional_Instructions());
+                testStepAssert.isTrue(addInstruction.length() <1,"Additional notes field should be empty","Additional notes field is empty","Additional notes field is not empty");
+                String additionalNotes = "Additional Notes";
+                Thread.sleep(1000);
+                String expectedAdditionalNotesTitle = action.getText(admin_EditScheduledBungiiPage.Label_AdditionalNotes());
+                testStepAssert.isFalse(expectedAdditionalNotesTitle.contentEquals(additionalNotes),"Special Instructions should be displayed", "Special Instructions is displayed","Special Instructions is not displayed");
+                Thread.sleep(1000);
+                break;
+        }
+    } catch(Exception e){
+        logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
+        error("Step should be successful", "Error performing step,Please check logs for more details",
+                true);
+    }
+    }
+
+    @And("^I remove the customer note$")
+    public void i_remove_the_customer_note() throws Throwable {
+        try{
+        Thread.sleep(1000);
+        action.clear(admin_EditScheduledBungiiPage.Text_Additional_Note());
+        log("I should be able to remove the customer note","I could remove the customer note",false);
+    } catch(Exception e){
+        logger.error("Error performing step", ExceptionUtils.getStackTrace(e));
+        error("Step should be successful", "Error performing step,Please check logs for more details",
+                true);
+    }
+    }
+
 
 }
--- Bungii.Test.Integration/src/test/resources/features/web/admin/Admin_Schedule_Delivery_Edit.feature
+++ Bungii.Test.Integration/src/test/resources/features/web/admin/Admin_Schedule_Delivery_Edit.feature
@@ -14,7 +14,7 @@ Feature: Admin_Schedule_Delivery_Edit
       And I wait for "2" mins
       Then I should be able to see the respective bungii with the below status
         |  Status |
-        | Searching Drivers |
+        | Assigning Driver(s) |
       Then I check the price for delivery
       When I click on "Edit" link beside scheduled bungii
       And I click on "Edit Trip Details" radiobutton
@@ -28,7 +28,7 @@ Feature: Admin_Schedule_Delivery_Edit
       And I wait for "2" mins
       When I view the delivery details in admin portal
       Then the updated drop off address should be displayed on delivery details page
-      And I confirm Pickup note is updated
+      And I confirm Pickup note is "Updated"
       And Delivery price is recalculated based on updated value of drop off address
 
   @regression
@@ -46,6 +46,7 @@ Feature: Admin_Schedule_Delivery_Edit
     And I click on "Edit Trip Details" radiobutton
     And I edit the drop off address
     Then I change the drop off address to "4400 Massachusetts Avenue Northwest"
+#    And I remove the customer note
     And I click on "Verify" button on Edit Scheduled bungii popup
       #Then Tick mark should be displayed beside driver and scheduled date
     When I click on "Save" button on Edit Scheduled bungii popup
@@ -53,6 +54,7 @@ Feature: Admin_Schedule_Delivery_Edit
     And I wait for "2" mins
     When I view the delivery details in admin portal
     Then the updated drop off address should be displayed on delivery details page
+#    And I confirm Pickup note is "Deleted"
     And Delivery price is recalculated based on updated value of drop off address
   
   @regression
@@ -139,5 +141,60 @@ Feature: Admin_Schedule_Delivery_Edit
        And Delivery price is recalculated based on updated value of drop off address
 
 
+  @ready
+  Scenario: Verify that additional notes textbox is not displayed for Partner portal on Schedule deliveries edit page on admin portal
+    When I navigate to "Partner" portal configured for "normal" URL
+    And I enter "valid" password on Partner Portal
+    And I click "SIGN IN" button on Partner Portal
+    Then I should "be logged in"
+    When I request "Solo" Bungii trip in partner portal configured for "normal" in "washingtondc" geofence
+      | Pickup_Address                                                                     | Delivery_Address                                                    |Load_Unload_Time|
+      | 601 13th Street Northwest, Washington, United States, District of Columbia, 20005  | 234 13th Street Northeast, Washington, District of Columbia 20002   |30 minutes      |
+    And I select Next Possible Pickup Date and Pickup Time
+      |Trip_Time            |
+      |NEXT_POSSIBLE        |
+    And I click "GET ESTIMATE" button on Partner Portal
+    Then I should see "Estimated Cost"
+    And I click "Continue" button on Partner Portal
+    Then I should "see Delivery Details screen"
+    When I enter following details on "Delivery Details" for "normal" on partner screen
+      |Items_To_Deliver|Customer_Name        |Customer_Mobile|Pickup_Contact_Name|Pickup_Contact_Phone|
+      |Furniture       |Testpartner A      |9998881111     |Test Pickup        |9999999359          |
+    And I Select "Customer Card" as Payment Method
+    And I enter following Credit Card details on Partner Portal
+      |CardNo   |Expiry |Postal_Code      |Cvv      |
+      |VISA CARD3|12/29  |VALID POSTAL CODE|VALID CVV|
+    And I click "Schedule Bungii" button on Partner Portal
+    Then I should "see Done screen"
+    And I am logged in as Admin
+    And I wait for "2" mins
+    And I view the all Scheduled Deliveries list on the admin portal
+    Then I should be able to see the respective bungii with the below status
+      |  Status |
+      |  Assigning Driver(s)  |
+    When I click on "Edit" link beside scheduled bungii
+    And I click on "Edit Trip Details" radiobutton
+    Then I should see "Additional Instructions" field empty
 
 
+  @ready
+  Scenario: To verify admin is able to add notes for customer trips in admin portal when customer has not added any additional notes
+    When I request "Solo Scheduled" Bungii as a customer in "washingtondc" geofence
+      | Bungii Time   | Customer Phone | Customer Name                         | Customer Note|
+      | NEXT_POSSIBLE | 9999999200     | Testcustomertywd_appleNewM Customer  |      Blank    |
+    And I view the all Scheduled Deliveries list on the admin portal
+    And I wait for "2" mins
+    Then I should be able to see the respective bungii with the below status
+      |  Status |
+      | Assigning Driver(s) |
+    Then I check the price for delivery
+    When I click on "Edit" link beside scheduled bungii
+    And I click on "Edit Trip Details" radiobutton
+    Then I should see "Additional Notes" field empty
+    And I change the customer note to "New Note Added by Admin"
+    And I click on "Verify" button on Edit Scheduled bungii popup
+    When I click on "Save" button on Edit Scheduled bungii popup
+    Then "Bungii Saved!" message should be displayed
+    And I wait for "2" mins
+    When I view the delivery details in admin portal
+    And I confirm Pickup note is "Added"
