a89a78f93d0d885bbabb6f5ca536bcac5f8b3855
CREATIVECAPSULE\krishna <krishna.hoderker@creativecapsule.com>
2022-05-25 17:31:15 +0530

    Checkin steps logger

--- Bungii.Test.Integration/src/main/java/com/bungii/SetupManager.java
+++ Bungii.Test.Integration/src/main/java/com/bungii/SetupManager.java
@@ -315,7 +315,7 @@ public class SetupManager extends EventFiringWebDriver {
         chromeOptions.setExperimentalOption("prefs", prefs);
         chromeOptions.addArguments("--no-sandbox");
          //if (PropertyUtility.getProp("target.platform").equalsIgnoreCase("IOS")) {
-         // chromeOptions.addArguments("--headless");
+          chromeOptions.addArguments("--headless");
           chromeOptions.addArguments("--window-size=1920,1080");
              chromeOptions.addArguments("--disable-dev-shm-usage");
              chromeOptions.addArguments("--disable-gpu");
