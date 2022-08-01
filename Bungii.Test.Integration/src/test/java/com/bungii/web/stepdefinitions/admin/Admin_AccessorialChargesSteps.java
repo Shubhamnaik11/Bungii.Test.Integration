8a603cd8dffb0d15e99d49216a3bcd618e61b1f4
CREATIVECAPSULE\krishna <krishna.hoderker@creativecapsule.com>
2022-08-01 11:02:11 +0530

    QA checkin - Feature new user data

--- Bungii.Test.Integration/src/test/java/com/bungii/web/stepdefinitions/admin/Admin_AccessorialChargesSteps.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/web/stepdefinitions/admin/Admin_AccessorialChargesSteps.java
@@ -22,6 +22,7 @@ import io.cucumber.datatable.DataTable;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.openqa.selenium.Keys;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
