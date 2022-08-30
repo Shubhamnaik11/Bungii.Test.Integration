f5ff41e22f0f736992884664dfceef550bb0bf4d
allan.fernandes <allan.fernandes@creativecapsule.com>
2022-08-30 15:16:26 +0530

    QA checkin - removed repeated elements

--- Bungii.Test.Integration/src/test/java/com/bungii/ios/pages/admin/ScheduledTripsPage.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/ios/pages/admin/ScheduledTripsPage.java
@@ -125,8 +125,6 @@ public class ScheduledTripsPage extends PageBase {
     public WebElement Text_CustomerHelpMessage() { return findElement("The customer will help you unload the item(s)", LocatorType.AccessibilityId); }
     public WebElement Label_DuoLift() { return findElement("DUO LIFT", LocatorType.AccessibilityId); }
     public WebElement Text_DuoLiftMessage() { return findElement("You and your duo teammate are responsible for loading and unloading the item(s)", LocatorType.AccessibilityId); }
-    public WebElement Link_DeliveryDetails(){return  findElement("dLabel",LocatorType.Id);}
-    public WebElement List_ViewDeliveries(){return  findElement("//td/div[@class='dropdown open']/ul/li/*[contains(text(),'Delivery Details')]",LocatorType.XPath);}
     public WebElement Icon_CustomerHelpAdminPortal(){return  findElement("//div/span[text() =\"Customer help\"]",LocatorType.XPath);}
     public WebElement Button_Duo(){return  findElement("duo",LocatorType.Id);}
     public WebElement Text_PickupTime() { return findElement("//label[text() =\"Pickup Time\"]/following-sibling::div[1]/div/div/div",LocatorType.XPath);}
