e1f33bfc65c5ddbdb497597bded95568215a28e4
allan.fernandes <allan.fernandes@creativecapsule.com>
2022-09-27 13:22:52 +0530

    QA checkin -removed extra spaces

--- Bungii.Test.Integration/src/test/java/com/bungii/ios/pages/driver/UpdateStatusPage.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/ios/pages/driver/UpdateStatusPage.java
@@ -92,9 +92,9 @@ public class UpdateStatusPage extends PageBase {
 	public WebElement Icon_DeliveryInstructions(){return findElement("//XCUIElementTypeImage[@name='slide_to_start_button']/parent::XCUIElementTypeOther/following-sibling::XCUIElementTypeOther/XCUIElementTypeOther[1]",LocatorType.XPath);}
 	public WebElement Icon_ItemDetails(){return findElement("//XCUIElementTypeImage[@name='slide_to_start_button']/parent::XCUIElementTypeOther/following-sibling::XCUIElementTypeOther/XCUIElementTypeOther[2]",LocatorType.XPath);}
 	public WebElement Icon_BungiiSupport(){return findElement("//XCUIElementTypeImage[@name='slide_to_start_button']/parent::XCUIElementTypeOther/following-sibling::XCUIElementTypeOther/XCUIElementTypeOther[3]",LocatorType.XPath);}
-    public WebElement Icon_Call(){return findElement("icon-text-large",LocatorType.AccessibilityId);}
-    public WebElement Icon_Text(){return findElement("icon-call-large",LocatorType.AccessibilityId);}
+	public WebElement Icon_Call(){return findElement("icon-text-large",LocatorType.AccessibilityId);}
+	public WebElement Icon_Text(){return findElement("icon-call-large",LocatorType.AccessibilityId);}
 	public WebElement Icon_Pickup(){return findElement("//XCUIElementTypeImage[@name=\"icon-stop-pickup\"]",LocatorType.XPath);}
-    public WebElement Icon_DropOff(){return findElement("//XCUIElementTypeImage[@name=\"icon-stop-drop-off\"]",LocatorType.XPath);}
+	public WebElement Icon_DropOff(){return findElement("//XCUIElementTypeImage[@name=\"icon-stop-drop-off\"]",LocatorType.XPath);}
 
 }
