0afad472b0c1c2af9aaa8d43c71a62aa67740bf3
sneha.naik <sneha.naik@creativecapsule.com>
2022-10-11 18:06:36 +0530

    Admin portal migration v2 - business users

--- Bungii.Test.Integration/src/test/java/com/bungii/web/pages/admin/Admin_BusinessUsersPage.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/web/pages/admin/Admin_BusinessUsersPage.java
@@ -5,30 +5,29 @@ import org.openqa.selenium.WebElement;
 
 public class Admin_BusinessUsersPage extends PageBase {
 
-    public WebElement Menu_BusinessUsers () { return findElement("adminmenu-businessuser", LocatorType.Id); }
+    public WebElement Menu_BusinessUsers () { return findElement("//span[contains(text(),'Bulk Delivery Upload')]", LocatorType.XPath); }
 
     public WebElement Menu_BusinessUsersPayment () { return findElement("adminmenu-businessusers-payment", LocatorType.Id); }
 
     public WebElement Menu_BulkTrips () { return findElement("adminmenu-businessusers-bulkbungii", LocatorType.Id); }
 
-    public WebElement Header_BusinessUsers () { return findElement("adminmenu-businessuser", LocatorType.Id); }
-//    public WebElement Header_BusinessUsers () { return findElement("//h4[text()='Business Users']", LocatorType.XPath); }
+    public WebElement Header_BusinessUsers () { return findElement("//h4[contains(text(),'Partners')]", LocatorType.XPath); }
 
-    public WebElement Button_CreateBusinessUser ( ) { return findElement("btnCreateBusinessUser", LocatorType.Id); }
+    public WebElement Button_CreateBusinessUser ( ) { return findElement("//button[contains(text(),'New Partner')]", LocatorType.XPath); }
 
     public WebElement TextBox_Search ( ) { return findElement("SearchCriteria", LocatorType.Id); }
 
-    public WebElement Button_Cancel ( ) { return findElement("//button[text()='Cancel']", LocatorType.XPath); }
+    public WebElement Button_Cancel ( ) { return findElement("//button[contains(text(),'Cancel')]", LocatorType.XPath); }
 
-    public WebElement Button_Save ( ) { return findElement("btnSaveBusinessUser", LocatorType.Id); }
+    public WebElement Button_Save ( ) { return findElement("//button[contains(text(),'Save')]", LocatorType.XPath); }
 
-    public WebElement TextBox_BusinessUserName( ) { return findElement("BusinessUser_Name", LocatorType.Id); }
+    public WebElement TextBox_BusinessUserName( ) { return findElement("//label[contains(text(),'Name:')]/parent::div/div/div/input", LocatorType.XPath); }
 
-    public WebElement DropDown_BusinessUserIsActive( ) { return findElement("BusinessUser_IsActive", LocatorType.Id); }
+    public WebElement DropDown_BusinessUserIsActive( ) { return findElement("//label[contains(text(),'Status:')]/parent::div/select", LocatorType.XPath); }
 
-    public WebElement TextBox_BusinessUserPhoneNo ( ) { return findElement("BusinessUser_PhoneNo", LocatorType.Id); }
+    public WebElement TextBox_BusinessUserPhoneNo ( ) { return findElement("//label[contains(text(),'Phone number:')]/following-sibling::div/div/input", LocatorType.XPath); }
 
-    public WebElement TextBox_BusinessUserEmailAddress( ) { return findElement("BusinessUser_EmailAddress", LocatorType.Id); }
+    public WebElement TextBox_BusinessUserEmailAddress( ) { return findElement("//label[contains(text(),'Email:')]/following-sibling::div/div/input", LocatorType.XPath); }
 
     //public WebElement Label_ErrorContainer( ) { return findElement("error-summary-modal", LocatorType.Id); }
     public WebElement Label_ErrorContainer( ) { return findElement("//span[@class='field-validation-error error']", LocatorType.XPath); }
