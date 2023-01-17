3a3706253519212697a391fcd860efc5deeed7e5
allan.fernandes <allan.fernandes@creativecapsule.com>
2023-01-17 10:58:08 +0530

    QA checkin -removed unused code

--- Bungii.Test.Integration/src/test/java/com/bungii/web/stepdefinitions/partner/Partner_IntegrationSteps.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/web/stepdefinitions/partner/Partner_IntegrationSteps.java
@@ -94,9 +94,7 @@ public class Partner_IntegrationSteps extends DriverBase {
                         if(addressEnter.equalsIgnoreCase("CopyPaste")) {
                             String copyPickup = Pickup_Address + Keys.chord(Keys.CONTROL, "A") + Keys.chord(Keys.CONTROL, "C");
                             action.sendKeys(Page_Partner_Dashboard.Dropdown_Pickup_Address(), copyPickup + Keys.chord(Keys.CONTROL, "v"));
-                        action.click(Page_Partner_Dashboard.Dropdown_Pickup_Address());
-                        Thread.sleep(3000);
-                        action.click(Page_Partner_Dashboard.List_Pickup_Address());
+
                         }
                         else{
 //                            action.clearSendKeys(Page_Partner_Dashboard.Dropdown_Pickup_Address(), Pickup_Address + Keys.TAB);
@@ -113,10 +111,7 @@ public class Partner_IntegrationSteps extends DriverBase {
                         if(addressEnter.equalsIgnoreCase("CopyPaste")) {
                             String copyDelivery = Delivery_Address + Keys.chord(Keys.CONTROL, "A") + Keys.chord(Keys.CONTROL, "C");
                             action.sendKeys(Page_Partner_Dashboard.Dropdown_Delivery_Address(), copyDelivery + Keys.chord(Keys.CONTROL, "v"));
-                            action.click(Page_Partner_Dashboard.Dropdown_Delivery_Address());
-                        Thread.sleep(5000);
-                        action.click(Page_Partner_Dashboard.List_Delivery_Address());
-                        Thread.sleep(8000);
+
                         }else {
 //                            action.clearSendKeys(Page_Partner_Dashboard.Dropdown_Delivery_Address(), Delivery_Address + Keys.TAB);
                             action.sendKeys(Page_Partner_Dashboard.Dropdown_Delivery_Address(), Delivery_Address);
