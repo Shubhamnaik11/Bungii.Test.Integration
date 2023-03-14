497ca8931f9bddea0e931b5c65f9b02157be97fe
allan.fernandes <allan.fernandes@creativecapsule.com>
2023-03-14 14:41:52 +0530

    QA checkin -added missing login file

--- Bungii.Test.Integration/src/test/java/com/bungii/android/stepdefinitions/Driver/LoginSteps.java
+++ Bungii.Test.Integration/src/test/java/com/bungii/android/stepdefinitions/Driver/LoginSteps.java
@@ -702,6 +702,14 @@ public class LoginSteps extends DriverBase {
                     cucumberContextManager.setScenarioContext("DRIVER_1_PHONE", phone);
                     shouldLoginSucessful = true;
                     break;
+                case "testdrivertywd_appledc_a_drvap washingtonap":
+                    phone = PropertyUtility.getDataProperties("Washington.driver40.phone");
+                    SetupManager.getObject().restartApp(PropertyUtility.getProp("bundleId_Driver"));
+                    password = PropertyUtility.getDataProperties("baltimore.driver.password");
+                    cucumberContextManager.setScenarioContext("DRIVER_1", PropertyUtility.getDataProperties("Washington.driver40.name"));
+                    cucumberContextManager.setScenarioContext("DRIVER_1_PHONE", phone);
+                    shouldLoginSucessful = true;
+                    break;
                 default:
                     throw new Exception("Please specify valid input - Driver name sent in cucumber steps " + option);
             }
